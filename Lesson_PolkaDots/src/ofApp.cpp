6535c01d9b5bd8121fdac41f8e270445a026fd21
Eve Weinberg <NeverOdd@Eves-MacBook-Pro.local>
2015-04-08 20:50:41 -0400

    typography attempt1

--- Lesson_PolkaDots/src/ofApp.cpp
+++ Lesson_PolkaDots/src/ofApp.cpp
@@ -16,12 +16,12 @@ void ofApp::drawPinkPolkaCircle(float xPos, float yPos, float scale){
     CircRadius = 100; // why am i defining this here?
 
     
-    ofSetColor(0);
+    ofSetColor(myOutlines);
     ofNoFill();
     ofSetLineWidth(6);
     ofCircle(CircCenterBX,CircCenterBY,CircRadius); // centerx, centery, radius - this is drawing the black outline to be at 0,0,100 radius
     
-    ofSetColor( ofColor::pink); //now i'm doing the polka dots
+    ofSetColor(Pink); //now i'm doing the polka dots
     ofFill();
     
     
@@ -31,7 +31,7 @@ void ofApp::drawPinkPolkaCircle(float xPos, float yPos, float scale){
     
         for (int i = 0; i < ofGetHeight(); i = i + 20){ //scoot it over 20 each time
         for (int k = 0; k < ofGetWidth(); k = k + 20){ //scoot it over 20 each time
-        float distance = ofDist(CircCenterBY, CircCenterBX, i, k);
+        float distance = ofDist(CircCenterBX, CircCenterBY, i, k);
             if (distance < circleRadius-4){
                 if ((k/20) % 2 == 0) {     // is k even or not ?
                     ofCircle(k,i,4);
@@ -44,10 +44,7 @@ void ofApp::drawPinkPolkaCircle(float xPos, float yPos, float scale){
     
     //why are these polkadots not filling the circle?
     
-//    ofSetColor(0);
-//    ofNoFill();
-//    ofSetLineWidth(6);
-//    ofCircle(CircCenterBX, CircCenterBY, CircRadius);
+
     
      ofPopMatrix(); //now we're done, reset coordinates to normal
     
@@ -55,7 +52,7 @@ void ofApp::drawPinkPolkaCircle(float xPos, float yPos, float scale){
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-//    ofSetCircleResolution(100);
+
     //    ofEnableAntiAliasing();
     ofSetLineWidth(2);
     
@@ -70,7 +67,7 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
     
-    ofSetFrameRate(12);
+    ofSetFrameRate(6);
     counter =  1.0f;
     
 }
@@ -84,56 +81,120 @@ void ofApp::draw(){
     cairo_set_line_cap(cairo->getCairoContext(), CAIRO_LINE_CAP_ROUND);
     cairo_set_line_join(cairo->getCairoContext(), CAIRO_LINE_JOIN_ROUND);
     ofClear(0,0,0, 255);
+
     
     ofBackground(255);  // Clear the screen with a black color
-    ofSetColor(0);  // Set the drawing color to white
-    
 
-    ofSetColor( ofColor::lightBlue);
+  ofSetColor(Blue);
     ofFill();
     
 
     
-    for (int rows=0; rows<5; rows++) {
-        for (int cols=0; cols<10; cols++) {
-            drawPinkPolkaCircle( rows * 100, cols*100, 0.5);
-            
-        }
-    }
-
-    
-    
-    // draw polka dots inside a circle ----------------------------
-    
-//    float MyCircA, MyCircB;
-//    MyCircA = mouseX;
-//    MyCircB = mouseY;
-//    for (int i = 0; i < ofGetHeight(); i = i + 20){
-//        for (int k = 0; k < ofGetWidth(); k = k + 20){
-//            float circleX = MyCircB;
-//            float circleY = MyCircA;
-//            float circleRadius = 300;
-//            float distance = ofDist(circleX, circleY, i, k);
-//            if (distance < circleRadius-4){
-//                if ((k/20) % 2 == 0) {     // is k even or not ?
-//                    ofCircle(k,i,4);
-//                } else {
-//                    ofCircle(k,i + 10,4);
-//                }
-//            }
+//    for (int rows=0; rows<5; rows++) {
+//        for (int cols=0; cols<10; cols++) {
+//            drawPinkPolkaCircle( rows * 100, cols*100, 0.5);
+//            
 //        }
 //    }
     
     
+    ofPushMatrix();
+    ofTranslate(300, 50);
+    drawPinkPolkaCircle(0, 0, 0.5);
+    ofPopMatrix();
+    
+    //draw a U--------
+    
+    ofPushMatrix();
+    ofTranslate(350, 0);
+    ofSetColor(Blue);
+    ofFill();
+    ofRect(0,0,25,50);
+    ofRect(75, 0, 25, 50);
+    ofCircle(50,50,50);
+     ofPopMatrix();
+
+    
+    // draw an M --------------------------
+
+    ofNoFill();
+    ofSetColor(myOutlines);
+    ofSetLineWidth(MyOutlineWidth);
+    ofBeginShape();
+    ofVertex(0,100);
+    ofVertex(50,0);
+    ofVertex(100,100);
+    ofVertex(80,100);
+    ofVertex(50,35);
+    ofVertex(20,100);
+    ofVertex(0,100);
+    ofEndShape();
+    
+    ofPushMatrix();
+    ofTranslate(50, 00);
+    ofNoFill();
+    ofSetColor(myOutlines);
+    ofSetLineWidth(MyOutlineWidth);
+    ofBeginShape();
+    ofVertex(0,100);
+    ofVertex(50,0);
+    ofVertex(100,100);
+    ofVertex(80,100);
+    ofVertex(50,35);
+    ofVertex(20,100);
+    ofVertex(0,100);
+    ofEndShape();
+    ofPopMatrix();
+    
+    /// draw an i --------------------------
+    
+    ofPushMatrix();
+    ofTranslate(150, 00);
+    ofNoFill();
+    ofSetColor(myOutlines);
+    ofSetLineWidth(MyOutlineWidth);
+    ofBeginShape();
+    ofCurveVertex(10, 100);
+    ofCurveVertex(20, 40);
+    ofCurveVertex(0, 80);
+    ofCurveVertex(40, 20);
+    ofCurveVertex(40, 20);
+    ofCurveVertex(50, 20);
+    ofCurveVertex(90, 90);
+    ofCurveVertex(80, 100);
+    ofCurveVertex(60, 30);
+    ofCurveVertex(60, 30);
+    ofCurveVertex(50, 30);
+     ofCurveVertex(50, 30);
+    ofCurveVertex(10, 100);
+       ofCurveVertex(10, 100);
+    ofEndShape();
+    ofPopMatrix();
+
+    
+    // draw an H
+    
+    ofColor colorOne;
+    ofColor colorTwo;
+    
+
     
     //-------------draw a triangle-------------------
     ofFill();
-    ofSetColor( ofColor::lightBlue);
- 
+    ofSetColor(MedAqua);
+    
+    if (ofGetElapsedTimef() <= 10){
+    ofTriangle(10*ofGetElapsedTimef(),100,10*ofGetElapsedTimef()+50,150,10*ofGetElapsedTimef(),200);
+    }
+    
+  
+    
     ofPushMatrix();
-    ofTranslate(500, 500);
-    ofScale(.5, .5);
+    ofTranslate(500, 20);
+    ofScale(.2, .2);
     float triangleX = 100;
+    
+    ofSetColor(MedAqua);
     
     if (ofGetFrameNum() % 12 == 0){
         ofTriangle(100,100,150,150,100,200);
@@ -142,13 +203,13 @@ void ofApp::draw(){
         ofTriangle(100+20,100,150+20,150,100+20,200);
     }
     if (ofGetFrameNum() % 12 == 2){
-         ofTriangle(100+40,100,150+40,150,100+40,200);
+        ofTriangle(100+40,100,150+40,150,100+40,200);
     }
     if (ofGetFrameNum() % 12 == 3){
-         ofTriangle(100+60,100,150+60,150,100+60,200);
+        ofTriangle(100+60,100,150+60,150,100+60,200);
     }
     if (ofGetFrameNum() % 12 == 4){
-         ofTriangle(100+80,100,150+80,150,100+80,200);
+        ofTriangle(100+80,100,150+80,150,100+80,200);
     }
     if (ofGetFrameNum() % 12 == 5){
         ofTriangle(100+100,100,150+100,150,100+100,200);
@@ -163,25 +224,63 @@ void ofApp::draw(){
         ofTriangle(100+180,100,150+180,150,100+180,200);
     }
     if (ofGetFrameNum() % 12 == 9){
-        ofTriangle(100+200,100,150+200,150,100+200,200);
-    }
-    if (ofGetFrameNum() % 12 == 10){
         ofTriangle(100+220,100,150+220,150,100+220,200);
     }
-    if (ofGetFrameNum() % 12 == 11){
+    if (ofGetFrameNum() % 12 == 10){
         ofTriangle(100+240,100,150+240,150,100+240,200);
     }
-    else if (ofGetFrameNum() % 12 == 12){
-         ofTriangle(100+260,100,150+260,150,100+260,200);
+    if (ofGetFrameNum() % 12 == 11){
+        ofTriangle(100+270,100,150+270,150,100+270,200);
     }
+    else if (ofGetFrameNum() % 12 == 12){
+        ofTriangle(100+300,100,150+300,150,100+300,200);
+    }
+    
+    ofSetColor(Salmon);
+    
+    if (ofGetFrameNum() % 12 == 1){
+        ofTriangle(70,100,120,150,70,200);
+    }
+    if (ofGetFrameNum() % 12 == 2){
+        ofTriangle(100,100,150,150,100,200);
+    }
+    if (ofGetFrameNum() % 12 == 3){
+        ofTriangle(100+20,100,150+20,150,100+20,200);
+    }
+    if (ofGetFrameNum() % 12 == 4){
+        ofTriangle(100+40,100,150+40,150,100+40,200);
+    }
+    if (ofGetFrameNum() % 12 == 5){
+        ofTriangle(100+70,100,150+70,150,100+70,200);
+    }
+    if (ofGetFrameNum() % 12 == 6){
+        ofTriangle(100+100,100,150+100,150,100+100,200);
+    }
+    if (ofGetFrameNum() % 12 == 7){
+        ofTriangle(100+130,100,150+130,150,100+130,200);
+    }
+    if (ofGetFrameNum() % 12 == 8){
+        ofTriangle(100+160,100,150+160,150,100+160,200);
+    }
+    if (ofGetFrameNum() % 12 == 9){
+        ofTriangle(100+180,100,150+180,150,100+180,200);
+    }
+    if (ofGetFrameNum() % 12 == 10){
+        ofTriangle(100+200,100,150+200,150,100+200,200);
+    }
+    if (ofGetFrameNum() % 12 == 11){
+        ofTriangle(100+220,100,150+220,150,100+220,200);
+    }
+    if (ofGetFrameNum() % 12 == 12){
+        ofTriangle(100+240,100,150+240,150,100+240,200);
+    }
+    else if (ofGetFrameNum() % 12 == 0){
+        ofTriangle(100+260,100,150+260,150,100+260,200);
+    }
+
     
     
     ofPopMatrix();
-    ofNoFill();
-    
-    ofSetColor( ofColor::black);
-    
-    // draw an animated line --------------------------
     
     float animateAtime = 1.0;
     float animateBtime = 1.0;  // assume that b is first
@@ -198,12 +297,24 @@ void ofApp::draw(){
                        animateAtime, 0,1, true);  // the minus animateBtime is cause b is assumed first
     float bpct = ofMap(timeInAnimation, 0,
                        animateBtime, 0,1, true);
-    float a = ofMap(apct, 0,1,50,400);
-    float b = ofMap(bpct, 0,1,50,400);
-    ofLine(700,a,700,b);
+    float cpct = ofMap(timeInAnimation+1, 0,
+                       animateBtime, 0,1, true);
+    float a = ofMap(apct, 0,1,0,100); // what do these numbers mean?
+    float b = ofMap(bpct, 0,1,0,100);
+    float c = ofMap(cpct, 0,1,0,100);
+    ofLine(600,b,600,c);
+    ofSetColor(Aqua);
+    ofLine(590,a,590,b);
+    ofSetColor(Salmon);
     ofLine(500,b,500,a);
-    ofLine(a+400,200,b+300,400);
-    ofLine(800,a,800,b);
+//    ofLine(c+500,60,b+500,60); // why why why?
+    ofLine(510,a,510,b);
+    
+    
+    
+    
+    
+    // ------------------------- finish cairo
     
     ofSetCurrentRenderer(opengl);
     tex.loadData(   cairo->getImageSurfacePixels().getPixels(),
--- Lesson_PolkaDots/src/ofApp.h
+++ Lesson_PolkaDots/src/ofApp.h
@@ -1,6 +1,15 @@
 #pragma once
+#ifndef _TEST_APP
+#define _TEST_APP
 
 #include "ofMain.h"
+
+struct Ball {
+    int x;
+    int y;
+    int vx;
+    int vy;
+};
 
 class ofApp : public ofBaseApp{
     
@@ -18,14 +27,18 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
-    
+        
     void DrawPolkaDotCircle( float xPos, float yPos, float scale);
     
     
     float counter, LineEnd, LineStart;
+    float MyOutlineWidth = 4;
     
       void drawPinkPolkaCircle(float xPos, float yPos, float scale);
-    // when do i use these floats? 
+    // when do i use these floats?
+    
+    Ball whiteBall;
+    Ball yellowBall;
     
     
     ofPtr<ofCairoRenderer> cairo;
@@ -33,7 +46,15 @@ public:
     ofTexture tex;
     
     ofColor myPurple = ofColor :: crimson;
-    ofColor myBlue = ofColor :: blue;
+    ofColor Pink = ofColor :: hotPink;
+    ofColor Blue = ofColor :: skyBlue;
+    ofColor myOutlines = ofColor :: black;
+    ofColor green = ofColor :: aquamarine;
+    ofColor Aqua = ofColor :: aquamarine;
+    ofColor MedAqua = ofColor :: mediumAquaMarine;
+    ofColor Salmon = ofColor :: salmon;
 };
+
+#endif
 
 
