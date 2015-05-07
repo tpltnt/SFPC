8a2b0b47b56d9505e046ef8713337d906b773cd3
tpltnt <tpltnt.github@dropcut.net>
2015-05-07 13:47:43 +0200

    fixed indentation & whitespace in ofApp.h

--- RuleTwo/src/ofApp.h
+++ RuleTwo/src/ofApp.h
@@ -23,30 +23,23 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-    
-    ofImage Head;
-    ofImage Ring;
-    ofImage GeneralDuties;
-    ofImage PullEverything;
 
-    ofColor colors[7];
-    ofPoint CirclePos[7];
-    
-    ofImage Art[7];
-     string str = "Art";
-    ofImage myImg;
-//    ofImage Art1, Art2, Art3, Art4, Art5, Art6, Art7;
-    float opactity[7];
-  
-    
-    
-    
-    // let's make a vector of the particles
-    vector <particle> particles;
-    vector <spring> springs;
-    
-    
-		
+        ofImage Head;
+        ofImage Ring;
+        ofImage GeneralDuties;
+        ofImage PullEverything;
+
+        ofColor colors[7];
+        ofPoint CirclePos[7];
+
+        ofImage Art[7];
+        string str = "Art";
+        ofImage myImg;
+        // ofImage Art1, Art2, Art3, Art4, Art5, Art6, Art7;
+        float opactity[7];
+
+        // let's make a vector of the particles
+        vector <particle> particles;
+        vector <spring> springs;		
 };
-
 #endif
